fa500b8f99e3dafd74e7614d5f9424763480f5be
Marissa Wilson <marfawilsoncode@gmail.com>
2024-03-13 09:46:42 -0500

    Added id to form element & created form variable

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Input values
+// Input variables
+let form = document.getElementById('form');
 let beforetax = document.getElementById('before-tax');
 let salestax = document.getElementById('tax-rate');
 let aftertax = document.getElementById('after-tax');
