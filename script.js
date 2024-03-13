8c6ffecf956686eceeff762d9b6876ead90cab33
Marissa Wilson <marfawilsoncode@gmail.com>
2024-03-13 09:41:22 -0500

    Created variables for form inputs

---
+++ script.js
@@ -0,0 +1,6 @@
+// Input values
+let beforetax = document.getElementById('before-tax');
+let salestax = document.getElementById('tax-rate');
+let aftertax = document.getElementById('after-tax');
+
+
