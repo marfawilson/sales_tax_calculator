b5c8e0c8dba796b5880b432ecce38784ddb9f75f
Marissa Wilson <marfawilsoncode@gmail.com>
2024-03-13 10:52:45 -0500

    Removed unneccessary whitespace

--- script.js
+++ script.js
@@ -3,5 +3,3 @@ let form = document.getElementById('form');
 let beforetax = document.getElementById('before-tax');
 let salestax = document.getElementById('tax-rate');
 let aftertax = document.getElementById('after-tax');
-
-
